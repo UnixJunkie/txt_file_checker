f1f0c7651c3765e93997fd8f3da91aca5a8b4b53
Francois Berenger <ligand@free.fr>
2025-02-28 15:05:06 +0000

    Smi_enforce: ok for tests

--- src/smi_enforce.ml
+++ src/smi_enforce.ml
@@ -4,14 +4,14 @@ module LO = Line_oriented
 module Log = Dolog.Log
 module S = BatString
 
-let input_fn = Sys.argv[1]
+let input_fn = Sys.argv.(1)
 
 let main () =
   Log.color_on ();
   Log.(set_log_level INFO);
   LO.iteri input_fn (fun i line ->
       try
-        let smi, name = Scanf.sscanf "%s@\t%s" line in
+        let smi, name = Scanf.sscanf line "%s@\t%s" (fun x y -> x, y) in
         assert(S.length smi > 0 &&
                S.length name > 0 &&
                S.count_char line '\t' == 1)
