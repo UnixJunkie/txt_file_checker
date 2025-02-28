922e442fdaacbd570646825bf8051db7e3dce559
Francois Berenger <ligand@free.fr>
2025-02-28 14:52:20 +0000

    new file:   src/smi_enforce.ml

---
+++ src/smi_enforce.ml
@@ -0,0 +1,23 @@
+(* enforce proper SMILES format *)
+
+module LO = Line_oriented
+module Log = Dolog.Log
+module S = BatString
+
+let input_fn = Sys.argv[1]
+
+let main () =
+  Log.color_on ();
+  Log.(set_log_level INFO);
+  LO.iteri input_fn (fun i line ->
+      try
+        let smi, name = Scanf.sscanf "%s@\t%s" line in
+        assert(S.length smi > 0 &&
+               S.length name > 0 &&
+               S.count_char line '\t' == 1)
+      with exn ->
+        let () = Log.error "line %d incorrect: %s" i line in
+        raise exn
+    )
+
+let () = main ()
